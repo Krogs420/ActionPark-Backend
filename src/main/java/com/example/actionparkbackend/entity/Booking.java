729d01ac4e71e95e9a651b4362cfc6f337ef2239
Kristian Kryger <kristianlaukryger@gmail.com>
2022-03-14 14:51:20 +0100

    Made Booking Class

--- src/main/java/com/example/actionparkbackend/entity/Booking.java
+++ src/main/java/com/example/actionparkbackend/entity/Booking.java
@@ -1,4 +1,20 @@
 package com.example.actionparkbackend.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bookingId;
+    private LocalDate creationDate = LocalDate.now();
+    private LocalDate bookingDate;
+    private int contenderAmount;
+
+
+
 }
