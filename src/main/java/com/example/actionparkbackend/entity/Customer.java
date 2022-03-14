94dd67fd2c2945e271fcaf9480a244ea83113744
Kristian Kryger <kristianlaukryger@gmail.com>
2022-03-14 14:58:37 +0100

    Made Customer Class

--- src/main/java/com/example/actionparkbackend/entity/Customer.java
+++ src/main/java/com/example/actionparkbackend/entity/Customer.java
@@ -1,10 +1,62 @@
 package com.example.actionparkbackend.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Customer {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customerId;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerMail;
+    private String customerPhoneNum;
+    private String customerAddress;
 
+    public int getCustomerId() {
+        return customerId;
+    }
+
+       public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerMail() {
+        return customerMail;
+    }
+
+    public void setCustomerMail(String customerMail) {
+        this.customerMail = customerMail;
+    }
+
+    public String getCustomerPhoneNum() {
+        return customerPhoneNum;
+    }
+
+    public void setCustomerPhoneNum(String customerPhoneNum) {
+        this.customerPhoneNum = customerPhoneNum;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
 }
