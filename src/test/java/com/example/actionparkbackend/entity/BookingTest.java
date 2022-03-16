6fe31e45f192ed0fb3c4f8ef9720ed78e201da7e
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-03-16 16:03:42 +0100

    added tests for booking for total amount

--- src/test/java/com/example/actionparkbackend/entity/BookingTest.java
+++ src/test/java/com/example/actionparkbackend/entity/BookingTest.java
@@ -1,6 +1,8 @@
 package com.example.actionparkbackend.entity;
 
+import com.example.actionparkbackend.service.BookingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class BookingTest {
 
+  @Autowired
+  BookingService bookingService;
+
   @Test
-  public void getTotalPrice() {
+  public void testGetTotalPricePass() {
+    Booking booking = bookingService.getBookingById(1);
+
+    assertEquals(300, booking.getTotalPrice());
   }
+
+  @Test
+  public void testGetTotalPriceFail() {
+    Booking booking = bookingService.getBookingById(1);
+
+    assertNotEquals(200, booking.getTotalPrice());
+  }
+
 }
