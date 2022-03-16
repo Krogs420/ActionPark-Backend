a409a40832968b6e8f6f1b6efd3a460ece44b4cc
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-03-16 17:12:02 +0100

    added post and put request for activities

--- src/main/java/com/example/actionparkbackend/controller/ActivityRESTController.java
+++ src/main/java/com/example/actionparkbackend/controller/ActivityRESTController.java
@@ -4,11 +4,19 @@ package com.example.actionparkbackend.controller;
 import com.example.actionparkbackend.entity.Activity;
 import com.example.actionparkbackend.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ActivityRESTController {
@@ -26,4 +34,21 @@ public class ActivityRESTController {
     return activityService.getActivityById(id);
   }
 
+  @PostMapping("/postActivity")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Activity postActivity(@RequestBody Activity activity){
+    return activityService.postNewActivity(activity);
+  }
+
+  @PutMapping("/activity/{id}")
+  public ResponseEntity<Activity> updateActivity(@PathVariable int id, @RequestBody Activity activity){
+    Optional<Activity> optionalActivity = activityService.findById(id);
+    if (optionalActivity.isPresent()){
+      activityService.saveActivity(activity);
+      return new ResponseEntity<>(activity, HttpStatus.OK);
+    } else{
+      return new ResponseEntity<>(activity, HttpStatus.NOT_FOUND);
+    }
+
+  }
 }
