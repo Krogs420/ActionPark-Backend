b962f2024e0a614e400eaf21ccff44c040ffa5ef
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-03-16 15:56:48 +0100

    added tests for booking line amount

--- src/test/java/com/example/actionparkbackend/entity/BookingLineTest.java
+++ src/test/java/com/example/actionparkbackend/entity/BookingLineTest.java
@@ -1,15 +1,29 @@
 package com.example.actionparkbackend.entity;
 
+import com.example.actionparkbackend.service.BookingLineService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class BookingLineTest {
 
-  @Test
-  public void testLineAmount(){
+  @Autowired
+  BookingLineService bookingLineService;
 
+  @Test
+  public void testLineAmountPass(){
+    BookingLine bookingLine = bookingLineService.getBookingLineById(1);
+
+    assertEquals(100, bookingLine.getLineAmount());
+  }
+
+  @Test
+  public void testLineAmountFail(){
+    BookingLine bookingLine = bookingLineService.getBookingLineById(1);
+
+    assertNotEquals(200, bookingLine.getLineAmount());
   }
 
 }
---
+++ src/test/java/com/example/actionparkbackend/entity/BookingTest.java
@@ -0,0 +1,14 @@
+package com.example.actionparkbackend.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BookingTest {
+
+  @Test
+  public void getTotalPrice() {
+  }
+}
