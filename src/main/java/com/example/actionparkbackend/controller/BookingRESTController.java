27ed9d9201cda9bb98a813db62121c0d4ef37d39
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-03-20 10:16:18 +0100

    refactored booking controller

--- src/main/java/com/example/actionparkbackend/controller/BookingRESTController.java
+++ src/main/java/com/example/actionparkbackend/controller/BookingRESTController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,28 +20,29 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("api/booking")
 public class BookingRESTController {
 
   @Autowired
   BookingService bookingService;
 
-  @GetMapping("/getBookings")
+  @GetMapping("/all-bookings")
   public List<Booking> getAllBooking() {
     return bookingService.getBookings();
   }
 
-  @GetMapping("/getBooking/{id}")
+  @GetMapping("/{id}")
   public Booking getBooking(@PathVariable int id) {
     return bookingService.getBookingById(id);
   }
 
-  @PostMapping("/postBooking")
+  @PostMapping("/add")
   @ResponseStatus(HttpStatus.CREATED)
   public Booking postBooking(@RequestBody Booking booking) {
     return bookingService.createNewBooking(booking);
   }
 
-  @PutMapping("booking/{id}")
+  @PutMapping("/update/{id}")
   public ResponseEntity<Booking> updateBooking(@PathVariable int id, @RequestBody Booking booking) {
     Optional<Booking> optionalBooking = bookingService.findById(id);
     if (optionalBooking.isPresent()) {
@@ -51,7 +53,7 @@ public class BookingRESTController {
     }
   }
 
-  @DeleteMapping("booking/{id}")
+  @DeleteMapping("/delete/{id}")
   public ResponseEntity<String> deleteBooking(@PathVariable int id) {
     try {
       bookingService.deleteBooking(id);
