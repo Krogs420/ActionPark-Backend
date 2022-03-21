00ccf910c0b730f32ecb30ef53ebf576a41e6fef
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-03-21 21:49:02 +0100

    small changes to be able to run controllers :q
    quit
    q;
    exit;
    exit:
    q:

--- src/main/java/com/example/actionparkbackend/controller/BookingLineRESTController.java
+++ src/main/java/com/example/actionparkbackend/controller/BookingLineRESTController.java
@@ -8,6 +8,7 @@ import com.example.actionparkbackend.service.BookingLineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/booking-line")
+@CrossOrigin
 public class BookingLineRESTController {
 
   @Autowired
