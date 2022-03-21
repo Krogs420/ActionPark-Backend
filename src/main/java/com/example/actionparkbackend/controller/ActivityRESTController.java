d1a771f8baca6f0d171094674ff5d15f93688a83
August <Augusthauerslev@outlook.com>
2022-03-21 10:52:54 +0100

    tilføjede image attribut og crossorigin

--- src/main/java/com/example/actionparkbackend/controller/ActivityRESTController.java
+++ src/main/java/com/example/actionparkbackend/controller/ActivityRESTController.java
@@ -6,21 +6,13 @@ import com.example.actionparkbackend.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/activity")
 public class ActivityRESTController {
 
--- src/main/java/com/example/actionparkbackend/entity/Activity.java
+++ src/main/java/com/example/actionparkbackend/entity/Activity.java
@@ -7,55 +7,60 @@ import java.util.List;
 
 @Entity
 public class Activity {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int activityId;
-  private String activityName;
-  private double activityPrice;
-  private String activityDescription;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int activityId;
+    private String activityName;
+    private double activityPrice;
+    private String activityDescription;
+    private String activityImageHref;
 
-  @OneToOne
-  @JoinColumn(name = "activity_id")
-  @JsonBackReference
-  private BookingLine bookingLine;
+    @OneToOne
+    @JoinColumn(name = "activity_id")
+    @JsonBackReference
+    private BookingLine bookingLine;
 
-  public int getActivityId() {
-    return activityId;
-  }
+    public int getActivityId() {
+        return activityId;
+    }
 
-  public String getActivityName() {
-    return activityName;
-  }
+    public String getActivityName() {
+        return activityName;
+    }
 
-  public void setActivityName(String activityName) {
-    this.activityName = activityName;
-  }
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
 
-  public double getActivityPrice() {
-    return activityPrice;
-  }
+    public double getActivityPrice() {
+        return activityPrice;
+    }
 
-  public void setActivityPrice(double activityPrice) {
-    this.activityPrice = activityPrice;
-  }
+    public void setActivityPrice(double activityPrice) {
+        this.activityPrice = activityPrice;
+    }
 
-  public String getActivityDescription() {
-    return activityDescription;
-  }
+    public String getActivityDescription() {
+        return activityDescription;
+    }
 
-  public void setActivityDescription(String activityDescription) {
-    this.activityDescription = activityDescription;
-  }
+    public void setActivityDescription(String activityDescription) {
+        this.activityDescription = activityDescription;
+    }
 
-  public void setActivityId(int activityId) {
-    this.activityId = activityId;
-  }
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
 
-  public BookingLine getBookingLine() {
-    return bookingLine;
-  }
+    public BookingLine getBookingLine() {
+        return bookingLine;
+    }
 
-  public void setBookingLine(BookingLine bookingLine) {
-    this.bookingLine = bookingLine;
-  }
+    public void setBookingLine(BookingLine bookingLine) {
+        this.bookingLine = bookingLine;
+    }
+
+    public String getActivityImageHref() {return activityImageHref;}
+
+    public void setActivityImageHref(String activityImageHref) { this.activityImageHref = activityImageHref;}
 }
