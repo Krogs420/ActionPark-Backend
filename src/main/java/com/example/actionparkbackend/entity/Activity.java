a50239c2ab0124368db1da5238802bf9d861755c
August <Augusthauerslev@outlook.com>
2022-03-22 09:55:07 +0100

    Tilføjet instruktører + getter og setter

--- src/main/java/com/example/actionparkbackend/entity/Activity.java
+++ src/main/java/com/example/actionparkbackend/entity/Activity.java
@@ -3,6 +3,8 @@ package com.example.actionparkbackend.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -18,6 +20,27 @@ public class Activity {
   @JoinColumn(name = "activity_id")
   @JsonBackReference
   private BookingLine bookingLine;
+  public static ArrayList<String> instructorNames;
+
+
+
+  public static ArrayList<String> getInstructorNames() {
+    return instructorNames;
+  }
+
+  public static void setInstructorNames(ArrayList<String> instructorNames) {
+    instructorNames.add("Aage Aalson");
+    instructorNames.add("Bjarne Bæver");
+    instructorNames.add("Charles Charlie");
+    instructorNames.add("Daniel Dum");
+    instructorNames.add("Erik Elifsen");
+    instructorNames.add("Frederike Ferm");
+    instructorNames.add("George Gunner");
+    instructorNames.add("Henrik Høi");
+
+    Activity.instructorNames = instructorNames;
+
+  }
 
   public int getActivityId() {
     return activityId;
